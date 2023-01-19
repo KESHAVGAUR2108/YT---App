b96f4452afeca08e00e58d9e518194b9803a2ee0
Keshav Gaur <keshav Gaur>
2023-01-19 14:00:30 +0530

    Removed client_id

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -8,7 +8,6 @@ export default axios.create({
             part : 'snippet',
             maxResults : 5,
             key : KEY,
-            client_id : "648493965334-dtb8bcbeacobr0aomrkl8r4tslt6bkaa.apps.googleusercontent.com"
         }
     }
 );
